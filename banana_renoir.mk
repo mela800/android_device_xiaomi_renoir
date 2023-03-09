381a8baf62989dbc857fefee6c150ee14326bac2
Cyphound <cyphound@gmail.com>
2023-03-09 18:47:04 -0300

    renoir: BananaDroid bringup

--- banana_renoir.mk
+++ banana_renoir.mk
@@ -11,27 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Project-Elixir stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-IS_PHONE := true
-ELIXIR_MAINTAINER := Cyphound
-ELIXIR_BUILD_TYPE := UNOFFICIAL
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_QUICK_TAP := true
+# Inherit some common BananaDroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
+
+# Device Banana Flags.
+BANANA_MAINTAINER := Cyphound
+TARGET_ENABLE_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+BANANA_BUILD_TYPE := UNOFFICIAL
 
 # Device specific stuff
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := aosp_renoir
+PRODUCT_NAME := banana_renoir
 PRODUCT_SYSTEM_NAME := renoir_global
 PRODUCT_SYSTEM_DEVICE := renoir
-
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
