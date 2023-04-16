737480b34c213225cf6908dd24e5065f40a2596c
Cyphound <bayrongomez.cyp@gmail.com>
2023-04-16 14:13:25 -0400

    renoir: Add build for fingerprint and prop overrides

--- banana_renoir.mk
+++ banana_renoir.mk
@@ -15,12 +15,12 @@ $(call inherit-product, device/xiaomi/renoir/device.mk)
 $(call inherit-product, vendor/banana/config/common.mk)
 
 # Device Banana Flags.
+BANANA_BUILD_TYPE := OFFICIAL
 BANANA_MAINTAINER := Cyphound
 TARGET_ENABLE_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-BANANA_BUILD_TYPE := OFFICIAL
 
 # Device specific stuff
 PRODUCT_BRAND := Xiaomi
@@ -30,6 +30,13 @@ PRODUCT_MODEL := Mi 11 Lite 5G
 PRODUCT_NAME := banana_renoir
 PRODUCT_SYSTEM_NAME := renoir_global
 PRODUCT_SYSTEM_DEVICE := renoir
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="renoir-user 13 RKQ1.211001.001 V14.0.8.0.TKICNXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Xiaomi/renoir/renoir:13/RKQ1.211001.001/V14.0.8.0.TKICNXM:user/release-keys
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
